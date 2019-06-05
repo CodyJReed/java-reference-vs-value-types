9b589df97e28d0ce2522fff662263546c6e91e6b
Cody Reed <cody2j2reed@gmail.com>
2019-06-05 09:27:37 -0400

    ArrayList in class example

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,48 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     private static Scanner scanner  = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
+
     public static void main(String[] args) {
-        int[] testArray = {1,5,3,7,11,15};
-        System.out.println(Arrays.toString(testArray));
-        reverse(testArray);
-        System.out.println(Arrays.toString(testArray));
+
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while(!quit) {
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    searchForItem();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
+//        int[] testArray = {1,5,3,7,11,15};
+//        System.out.println(Arrays.toString(testArray));
+//        reverse(testArray);
+//        System.out.println(Arrays.toString(testArray));
 //        System.out.println("Enter count:");
 //        int count = scanner.nextInt();
 //        scanner.nextLine();
@@ -44,15 +78,58 @@ public class Main {
 //        System.out.println("anotherArray after modify = " + Arrays.toString(anotherArray));
     }
 
-    private static void reverse(int[] array) {
-        int maxIndex = array.length -1;
-        int halfLength = array.length / 2;
-        for(int i=0;i<halfLength; i++) {
-            int temp = array[i];
-            array[i] = array[maxIndex -i];
-            array[maxIndex -i] = temp;
-        }
+    public static void printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options. ");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item in the list.");
+        System.out.println("\t 6 - To quit the application.");
     }
+
+    public static void addItem() {
+        System.out.println("Please enter the grocery item: ");
+        groceryList.addGroceryItem(scanner.nextLine());
+    }
+
+    public static void modifyItem() {
+        System.out.print("Enter item number: ");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter replacement item: ");
+        String newItem = scanner.nextLine();
+        groceryList.modifyGroceryItem(itemNo-1, newItem);
+    }
+
+    public static void removeItem() {
+        System.out.println("Enter item number: ");
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        groceryList.removeGroceryItem(itemNo-1);
+    }
+
+    public static void searchForItem() {
+        System.out.print("Enter an item to search for: ");
+        String itemName = scanner.nextLine();
+        if(groceryList.findItem(itemName) != null) {
+            System.out.println("Found " + itemName + " in our grocery list");
+        } else {
+            System.out.println(itemName + " was not found in our grocery list");
+        }
+
+    }
+
+//    private static void reverse(int[] array) {
+//        int maxIndex = array.length -1;
+//        int halfLength = array.length / 2;
+//        for(int i=0;i<halfLength; i++) {
+//            int temp = array[i];
+//            array[i] = array[maxIndex -i];
+//            array[maxIndex -i] = temp;
+//        }
+//    }
 
 //    private static int[] readIntegers(int count) {
 //        int[] array = new int[count];
